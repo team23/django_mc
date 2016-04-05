e3019d0edb0e3fda7b126487532497586d319cb0
David Danier <danier@team23.de>
2016-04-05 10:39:22 +0200

    Started next version

--- django_mc/__init__.py
+++ django_mc/__init__.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ImproperlyConfigured
 from .mixins import *  # noqa
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev1'
 
 
 default_app_config = 'django_mc.apps.DjangoMCAppConfig'
